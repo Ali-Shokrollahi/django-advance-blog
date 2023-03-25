14a1810b26efd6b01ff181c78679c6bcf1dedc54
Ali Shokrollahi <ali.shokrollahi.pro@gmail.com>
2023-03-25 18:43:47 +0430

    add full_name to profile model.

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -57,3 +57,7 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.email
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
